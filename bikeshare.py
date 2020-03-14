842761b0c9658a0005be456fb2180ca31529d993
Tobias <tobias@heidrich-net.de>
2020-03-14 12:12:52 +0100

    add new text at the end of the bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -195,7 +195,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df, city)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart or quit? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
